26be4f01dbf8b54208578bf7ede438dea22f4029
DLongoni-vm <dave88@focusinformatica.com>
2017-06-13 01:08:58 +0200

    echo eventi multipli

--- html/get_concerts.php
+++ html/get_concerts.php
@@ -3,8 +3,8 @@ require_once __DIR__ . '/../src/REP/RepEvent.php';
 $rep=new RepEvent();
 $inDt = date_create_from_format('d/m/Y',$_REQUEST['date']);
 $inDt = date_format($inDt,"j-M-Y");
-$eO=RepEvent::getByDate($inDt)[0];
-if ($eO != NULL){
-  echo($eO->getHtml());
+$eList=RepEvent::getByDate($inDt);
+foreach ($eList as $e){
+  echo($e->getHtml());
 }
 ?>
