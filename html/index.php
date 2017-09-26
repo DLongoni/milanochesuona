8d9871880ad98f0b7173459a5acd177b94ff2315
DLongoni-home <dave88@focusinformatica.com>
2017-09-26 22:25:59 +0200

    salvattore

--- html/index.php
+++ html/index.php
@@ -7,6 +7,15 @@
     <title>MilanoCheSuona</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <style>
+      #grid[data-columns]::before {
+        content: '3 .column.size-1of3';
+      }
+
+      /* These are the classes that are going to be applied: */
+      .column { float: left; }
+      .size-1of3 { width: 33.333%; }
+    </style>
   </head>
   <body style="padding:2em">
     <div class="container">
@@ -32,7 +41,7 @@
         <div class="col-md-4"></div>
       </div>
     </div>
-    <div id="divConcerts" class="container m-t-2">
+    <div id="grid" data-columns>
 <?php
 // require_once __DIR__ . '/../src/REP/RepEvent.php';
 // $rep=new RepEvent();
@@ -40,12 +49,14 @@
 // echo($eO->getHtml());
 ?>
 </div>
+
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
     <script src="https://kswedberg.github.io/jquery-expander/jquery.expander.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/salvattore/1.0.9/salvattore.min.js"></script>
 <script>
 $(document).ready(
   function() {
