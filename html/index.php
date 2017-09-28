df6f680b8c2509e51a3909b3d6571a71297f9edf
davide-uff <dave88@focusinformatica.com>
2017-09-28 12:40:00 +0200

    esternalizzo css e js, commento js, pulisco index.php

--- html/index.php
+++ html/index.php
@@ -1,29 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+  <!-- HEAD {{{ -->
+  <head> 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>MilanoCheSuona</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-    <style>
-      #grid[data-columns]::before {
-        content: '3 .column.size-1of3';
-      }
 
-      /* These are the classes that are going to be applied: */
-      .column { float: left; }
-      .size-1of3 { width: 33.333%; }
-    </style>
-  </head>
+    <title>MilanoCheSuona</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" href='./lib/mcs.css'>
+
+  </head> 
+  <!-- }}} -->
+
+  <!-- BODY {{{ -->
   <body style="padding:2em">
     <div class="container">
       <div class="header">
         <h1 class="text-md-center">MilanoCheSuona</h1> 
         <p class="text-md-center text-muted">solo musica dal vivo</p> 
         <p class="text-md-center text-danger small">VERSIONE PROGETTO IN ALPHA TESTING</p> 
-        <!-- <button type="button" class="btn btn btn&#45;primary pull&#45;md&#45;right">Segnala concerto</button>   -->
       </div>
       <hr>
       <div class="row">
@@ -41,90 +39,17 @@
         </div>
         <div class="col-md-4"></div>
       </div>
-    <div id="grid" data-columns>
+      <div id="grid" data-columns></div>
     </div>
-<?php
-// require_once __DIR__ . '/../src/REP/RepEvent.php';
-// $rep=new RepEvent();
-// $eO=RepEvent::getByDate('13-sep-2016')[0];
-// echo($eO->getHtml());
-?>
-</div>
 
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
     <script src="https://kswedberg.github.io/jquery-expander/jquery.expander.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/salvattore/1.0.9/salvattore.min.js"></script>
-<script>
-$(document).ready(
-  function() {
-    $( "#txtDatePicker" ).datepicker(
-  {minDate: 0,
-  dateFormat: "dd/mm/yy",
-  showOtherMonths: true,
-  selectOtherMonths: true,
-  showAnim: 'slideDown',
-defaultDate: '0',
-onSelect: function(dtTxt){
-  loadConcerts(dtTxt);
-}
-  }).datepicker('setDate', new Date()); // to select today as default
-
-    $("#btnNext").click(function () {
-      var selDt = $('#txtDatePicker').datepicker("getDate");
-      selDt.setDate(selDt.getDate()+1);
-      $('#txtDatePicker').datepicker("setDate", selDt);
-      loadConcerts($('#txtDatePicker').datepicker().val());
-    });
-
-    $("#btnPrev").click(function () {
-      var selDt = $('#txtDatePicker').datepicker("getDate");
-      selDt.setDate(selDt.getDate()-1);
-      $('#txtDatePicker').datepicker("setDate", selDt);
-      loadConcerts($('#txtDatePicker').datepicker().val());
-    });
-  } );
-
-function loadConcerts(dt) {
-  $.ajax({
-  url:"/get_concerts.php",
-    data:"date="+dt,
-    type:"POST",
-    success:function(data){
-      var cards = jQuery.parseJSON(data);
-      var grid = document.getElementById('grid');
-      grid.innerHTML='';
-      salvattore.registerGrid(grid);
-      salvattore.recreateColumns(grid);
-      jQuery.each(cards, function(index,value){
-        var item = document.createElement('div');
-        salvattore['append_elements'](grid, [item]);
-        item.outerHTML = value;
-      });
-
-      $('.toolong').expander({
-      slicePoint: 200,
-        preserveWords: true,
-        widow: 10,
-        expandEffect: 'fadeIn',
-        collapseEffect: 'fadeOut',
-        expandText: ' leggi tutto',
-        expandPrefix: '...',
-        userCollapsePrefix: ' ',
-        userCollapseText: ' leggi meno'
-      });
-      $('.toolong').expander().removeClass('js-toolong-hidden');
-    },
-    error:function(xhr,ajaxOptions,thrownError){
-      alert(xhr.status);
-      alert(thrownError);
-    }
-});
-}
-
-</script>
+    <script src="./lib/mcs.js"></script>
   </body>
+  <!-- }}} -->
 </html>
---
+++ html/lib/mcs.js
@@ -0,0 +1,73 @@
+.$(document).ready(
+    function() {
+      // Date Picker
+      $( "#txtDatePicker" ).datepicker(
+          {minDate: 0,
+            dateFormat: "dd/mm/yy",
+            showOtherMonths: true,
+            selectOtherMonths: true,
+            showAnim: 'slideDown',
+            defaultDate: '0',
+            onSelect: function(dtTxt){
+              loadConcerts(dtTxt);
+            }
+          }).datepicker('setDate', new Date()); // to select today as default
+
+      $("#btnNext").click(function () {
+        var selDt = $('#txtDatePicker').datepicker("getDate");
+        selDt.setDate(selDt.getDate()+1);
+        $('#txtDatePicker').datepicker("setDate", selDt);
+        loadConcerts($('#txtDatePicker').datepicker().val());
+      });
+
+      $("#btnPrev").click(function () {
+        var selDt = $('#txtDatePicker').datepicker("getDate");
+        selDt.setDate(selDt.getDate()-1);
+        $('#txtDatePicker').datepicker("setDate", selDt);
+        loadConcerts($('#txtDatePicker').datepicker().val());
+      });
+    } );
+
+// Load Concerts - called on datepicker select or arrow click
+function loadConcerts(dt) {
+  $.ajax({
+    url:"/get_concerts.php",
+    data:"date="+dt,
+    type:"POST",
+    success:function(data){
+      // Setup the grid using salvattore
+      var cards = jQuery.parseJSON(data); // jquery array or strings representing cards
+      var grid = document.getElementById('grid');
+      grid.innerHTML='';
+      salvattore.registerGrid(grid);
+      salvattore.recreateColumns(grid);
+      jQuery.each(cards, function(index,value){
+        var item = document.createElement('div');
+        salvattore['append_elements'](grid, [item]);
+        item.outerHTML = value;
+      });
+
+      // Expander for event descriptions should stay inside here
+      $('.toolong').expander({
+        slicePoint: 200,
+        preserveWords: true,
+        widow: 10,
+        expandEffect: 'fadeIn',
+        collapseEffect: 'fadeOut',
+        expandText: ' leggi tutto',
+        expandPrefix: '...',
+        userCollapsePrefix: ' ',
+        userCollapseText: ' leggi meno'
+      });
+      $('.toolong').expander().removeClass('js-toolong-hidden');
+    },
+    error:function(xhr,ajaxOptions,thrownError){
+      // Uncomment only for debugging purposes
+      // alert(xhr.status);
+      // alert(thrownError);
+      alert('Errore imprevisto nel caricamento dati.');
+    }
+  });
+}
+
+
