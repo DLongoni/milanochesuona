caaa2d79631d8d4b4c3acaaaabf79b1826921730
DLongoni-vm <dave88@focusinformatica.com>
2017-11-11 17:55:04 +0100

    swipe on touchstart

--- html/lib/mcs.js
+++ html/lib/mcs.js
@@ -13,10 +13,24 @@ $(document)
   .ajaxStart(function () {
     $loadingOverlay.show();
   })
-  .ajaxStop(function () {
-    $loadingOverlay.hide();
-  });
+.ajaxStop(function () {
+  $loadingOverlay.hide();
+});
 // }}}
+
+$(document).on("touchstart",function(event) {
+  $("#grid .grid-item").swipe('destroy');
+  $("#grid .grid-item").swipe( {
+    swipeLeft:function(event, direction, distance, duration, fingerCount, fingerData){
+      swipeCallback($(this), direction, fingerCount);
+    },
+    swipeRight:function(event, direction, distance, duration, fingerCount, fingerData){
+      swipeCallback($(this), direction, fingerCount);
+    },
+    allowPageScroll:"vertical",
+    threshold:80
+  });
+});
 
 $(document).ready(
     function() {
@@ -72,20 +86,9 @@ $(document).ready(
             });
 
             $(".in-click").click( function(event, direction, distance, duration, fingerCount, fingerData){
-                window.open($(this).attr("t-link"));
+              window.open($(this).attr("t-link"));
             });
 
-            $("#grid .grid-item").swipe('destroy');
-            $("#grid .grid-item").swipe( {
-              swipeLeft:function(event, direction, distance, duration, fingerCount, fingerData){
-                swipeCallback($(this), direction, fingerCount);
-              },
-              swipeRight:function(event, direction, distance, duration, fingerCount, fingerData){
-                swipeCallback($(this), direction, fingerCount);
-              },
-              allowPageScroll:"vertical",
-              threshold:80
-            });
 
           },
           error:function(xhr,ajaxOptions,thrownError){
@@ -109,29 +112,29 @@ $(document).ready(
       });
       // }}}
 
-// {{{ REGION: Swipe Callback
-function swipeCallback($elem, direction, fingerCount) {
-  // Uncomment only when deployed on actual mobile devices
-  // if ((direction == "left" || direction == "right") && fingerCount == 1){
-  if ($(window).width() < 760){
-    var targetClass ="";
-    if ((direction == "left" || direction == "right") && fingerCount <= 1){
-      if (direction == "left"){
-        targetClass = "swipel";
+      // {{{ REGION: Swipe Callback
+      function swipeCallback($elem, direction, fingerCount) {
+        // Uncomment only when deployed on actual mobile devices
+        // if ((direction == "left" || direction == "right") && fingerCount == 1){
+        if ($(window).width() < 760){
+          var targetClass ="";
+          if ((direction == "left" || direction == "right") && fingerCount <= 1){
+            if (direction == "left"){
+              targetClass = "swipel";
+            }
+            if (direction == "right"){
+              targetClass = "swiper";
+            }
+            // $grid.isotope({transitionDuration:0});
+            $elem.addClass(targetClass).delay(80).queue(function(){
+              $grid.isotope('remove',$elem).dequeue();
+              // $grid.isotope({transitionDuration:'0.4s'});
+              $grid.isotope();
+            });
+          }
+        }
       }
-      if (direction == "right"){
-        targetClass = "swiper";
-      }
-      // $grid.isotope({transitionDuration:0});
-      $elem.addClass(targetClass).delay(80).queue(function(){
-        $grid.isotope('remove',$elem).dequeue();
-        // $grid.isotope({transitionDuration:'0.4s'});
-        $grid.isotope();
-      });
-    }
-  }
-}
-// }}}
+      // }}}
 
       // {{{ REGION: Initializations
       $('#modalSegnala').on('show.bs.modal',function(e){
@@ -200,204 +203,204 @@ function swipeCallback($elem, direction, fingerCount) {
       loadConcerts($datePicker.datepicker().val());
       // }}}
     } );
-// {{{ REGION: Filters
-var filterFunctions = [
-  function(elem) { // check milano
-    if((milanoHinterland[0] && elem.hasClass('milano')) ||
-        (milanoHinterland[1] && !elem.hasClass('milano'))) {
-      return true; }
-    else
-    { return false; }
-  },
-  function(elem) { //check loc
-    if(locFilters.length ==0){return true;}
-    for(var i=0;i<locFilters.length;i++){
-      if( elem.hasClass('loc-' + locFilters[i]) ){
-        return true;
+    // {{{ REGION: Filters
+    var filterFunctions = [
+      function(elem) { // check milano
+        if((milanoHinterland[0] && elem.hasClass('milano')) ||
+            (milanoHinterland[1] && !elem.hasClass('milano'))) {
+          return true; }
+        else
+        { return false; }
+      },
+      function(elem) { //check loc
+        if(locFilters.length ==0){return true;}
+        for(var i=0;i<locFilters.length;i++){
+          if( elem.hasClass('loc-' + locFilters[i]) ){
+            return true;
+          }
+        }
+        return false;
       }
-    }
-    return false;
-  }
-]
+    ]
 
-// Clic button Nswe -> set filter array and re-isotope
-$('#divNswe').on('click','button',function(event){
-  $target = $(event.currentTarget);
-  var isChecked = toggleClassAndIsChecked($target);
-  var f = $target.html().toLowerCase();
-  if (isChecked) {addLocFilter(f);}
-  else {removeLocFilter(f);}
-  setFilterCookies();
-  $grid.isotope();
-});
+      // Clic button Nswe -> set filter array and re-isotope
+      $('#divNswe').on('click','button',function(event){
+        $target = $(event.currentTarget);
+        var isChecked = toggleClassAndIsChecked($target);
+        var f = $target.html().toLowerCase();
+        if (isChecked) {addLocFilter(f);}
+        else {removeLocFilter(f);}
+        setFilterCookies();
+        $grid.isotope();
+      });
 
-// Clic button Mh -> set filter array and re-isotope
-$('#divMilanoHinterland').on('click','button',function(event){
-  $target = $(event.currentTarget);
-  var isChecked = toggleClassAndIsChecked($target);
-  if (isChecked) {var v = 1}
-  else {var v = 0}
-  if ($target.html() == "Milano"){
-    milanoHinterland[0] = v;
-  }
-  else{
-    milanoHinterland[1] = v;
-  }
-  setFilterCookies();
-  $grid.isotope();
-});
-
-function addLocFilter(filter) { // location filter add
-  if (locFilters.indexOf(filter)==-1)
-  { locFilters.push(filter); }
-}
-
-function removeLocFilter(filter) { // location filter del
-  var id = locFilters.indexOf(filter);
-  if (id !=-1)
-  { locFilters.splice(id,1); }
-}
-
-// toggle class and return whether is checked or not
-function toggleClassAndIsChecked($target){
-  if($target.hasClass('btn-sel')) { deSelectBtn($target); }
-  else { selectBtn($target); }
-  var isChecked = $target.hasClass('btn-sel');
-  return isChecked;
-}
-
-function deSelectBtn($b){
-  $b.removeClass('btn-sel').addClass('btn-not-sel');
-}
-
-function selectBtn($b){
-  $b.removeClass('btn-not-sel').addClass('btn-sel');
-}
-// }}}
-
-// {{{ REGION: Cookies
-function setFilterCookies() {
-  var d = new Date();
-  d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
-  var expires = "expires="+d.toUTCString();
-  // cookie will be locFilters|mhFilters
-  var filtersStr = locFilters.join(",") + "|" + milanoHinterland.join(",");
-  document.cookie = "filters=" + filtersStr + ";" + expires + ";path=/";
-}
-
-function setSortCookies() {
-  var d = new Date();
-  d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
-  var expires = "expires="+d.toUTCString();
-  document.cookie = "sort=" + sort + ";" + expires + ";path=/";
-}
-
-function setSortFromCookies(){
-  var filterS = cookieValue("sort");
-  sort = filterS;
-  isoSort();
-}
-
-function setFiltersFromCookies(){
-  var filterC = cookieValue("filters");
-  if (filterC != ""){
-    filtersArr = filterC.split("|");
-    if(filtersArr[0]!=""){
-      locFilterC = filtersArr[0].split(",");
-    }
+    // Clic button Mh -> set filter array and re-isotope
+    $('#divMilanoHinterland').on('click','button',function(event){
+      $target = $(event.currentTarget);
+      var isChecked = toggleClassAndIsChecked($target);
+      if (isChecked) {var v = 1}
+      else {var v = 0}
+      if ($target.html() == "Milano"){
+        milanoHinterland[0] = v;
+      }
       else{
-        locFilterC=[];
-    }
-    mhFilterC  = filtersArr[1].split(",");
-    if (locFilterC.length >0){ // Set loc filters
-      locFilters = locFilterC;
-      setNsweClasses();
-    }
-    if (mhFilterC.length >0){ // Set mh filters
-      milanoHinterland = mhFilterC;
-      setMhClasses();
-    }
-  }
-}
-
-function cookieValue(cName){
-  var name = cName + "=";
-  var ret = "";
-  var ca = document.cookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      ret = c.substring(name.length, c.length);
-    }
-  }
-  return ret;
-}
-
-function setNsweClasses(){
-  for(var i = 0 ;i<locFilters.length;i++){
-    $('#divNswe').children('button').each(function(){
-      if ($(this).html().toLowerCase()==locFilters[i]){
-        selectBtn($(this));
+        milanoHinterland[1] = v;
       }
+      setFilterCookies();
+      $grid.isotope();
     });
-  }
-}
 
-function setMhClasses(){
-  if(milanoHinterland[0]==0){
-    deSelectBtn($('#divMilanoHinterland #btnMilano'));
-  }
-  if(milanoHinterland[1]==0){
-    deSelectBtn($('#divMilanoHinterland #btnHinterland'));
-  }
-}
-// }}}
-
-// {{{ REGION: Sort
-$("#divSort button").click(function(){
-  toggleSort($(this));
-  isoSort();
-  setSortCookies();
-});
-
-function isoSort(){
-  var field = sort.charAt(0);
-  var asc = sort.charAt(1) == 'a';
-  $btnSort = $('#btnSort'+field);
-  var t = $btnSort.text().split(" ");
-  var tipo_ord = t[0];
-  var arrow_n = ((asc) ? '\u2191' : '\u2193');
-  $btnSort.text(tipo_ord + ' ' + arrow_n);
-  $btnSort.siblings().removeClass('btn-sel').addClass('btn-not-sel');
-  $btnSort.addClass('btn-sel').removeClass('btn-not-sel');
-  $grid.isotope({sortBy: field, sortAscending: asc});
-}
-
-function toggleSort($target){
-  var t = $target.text().split(" ");
-  var tipo_ord = t[0];
-  var sort_t = tipo_ord.charAt(0).toLowerCase();
-  var arrow_n = "";
-  if ($target.hasClass('btn-sel')){
-    if (t[1]=='\u2191'){
-      arrow_n = '\u2193';
+    function addLocFilter(filter) { // location filter add
+      if (locFilters.indexOf(filter)==-1)
+      { locFilters.push(filter); }
     }
-    else{
-      arrow_n = '\u2191';
+
+    function removeLocFilter(filter) { // location filter del
+      var id = locFilters.indexOf(filter);
+      if (id !=-1)
+      { locFilters.splice(id,1); }
     }
-  }
-  else{
-    arrow_n = t[1];
-  }
-  if (arrow_n =='\u2191'){
-    sort_o = 'a';
-  }
-  else{
-    sort_o = 'd';
-  }
-  sort = sort_t + sort_o;
-}
-// }}}
+
+    // toggle class and return whether is checked or not
+    function toggleClassAndIsChecked($target){
+      if($target.hasClass('btn-sel')) { deSelectBtn($target); }
+      else { selectBtn($target); }
+      var isChecked = $target.hasClass('btn-sel');
+      return isChecked;
+    }
+
+    function deSelectBtn($b){
+      $b.removeClass('btn-sel').addClass('btn-not-sel');
+    }
+
+    function selectBtn($b){
+      $b.removeClass('btn-not-sel').addClass('btn-sel');
+    }
+    // }}}
+
+    // {{{ REGION: Cookies
+    function setFilterCookies() {
+      var d = new Date();
+      d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+      var expires = "expires="+d.toUTCString();
+      // cookie will be locFilters|mhFilters
+      var filtersStr = locFilters.join(",") + "|" + milanoHinterland.join(",");
+      document.cookie = "filters=" + filtersStr + ";" + expires + ";path=/";
+    }
+
+    function setSortCookies() {
+      var d = new Date();
+      d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+      var expires = "expires="+d.toUTCString();
+      document.cookie = "sort=" + sort + ";" + expires + ";path=/";
+    }
+
+    function setSortFromCookies(){
+      var filterS = cookieValue("sort");
+      sort = filterS;
+      isoSort();
+    }
+
+    function setFiltersFromCookies(){
+      var filterC = cookieValue("filters");
+      if (filterC != ""){
+        filtersArr = filterC.split("|");
+        if(filtersArr[0]!=""){
+          locFilterC = filtersArr[0].split(",");
+        }
+        else{
+          locFilterC=[];
+        }
+        mhFilterC  = filtersArr[1].split(",");
+        if (locFilterC.length >0){ // Set loc filters
+          locFilters = locFilterC;
+          setNsweClasses();
+        }
+        if (mhFilterC.length >0){ // Set mh filters
+          milanoHinterland = mhFilterC;
+          setMhClasses();
+        }
+      }
+    }
+
+    function cookieValue(cName){
+      var name = cName + "=";
+      var ret = "";
+      var ca = document.cookie.split(';');
+      for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          ret = c.substring(name.length, c.length);
+        }
+      }
+      return ret;
+    }
+
+    function setNsweClasses(){
+      for(var i = 0 ;i<locFilters.length;i++){
+        $('#divNswe').children('button').each(function(){
+          if ($(this).html().toLowerCase()==locFilters[i]){
+            selectBtn($(this));
+          }
+        });
+      }
+    }
+
+    function setMhClasses(){
+      if(milanoHinterland[0]==0){
+        deSelectBtn($('#divMilanoHinterland #btnMilano'));
+      }
+      if(milanoHinterland[1]==0){
+        deSelectBtn($('#divMilanoHinterland #btnHinterland'));
+      }
+    }
+    // }}}
+
+    // {{{ REGION: Sort
+    $("#divSort button").click(function(){
+      toggleSort($(this));
+      isoSort();
+      setSortCookies();
+    });
+
+    function isoSort(){
+      var field = sort.charAt(0);
+      var asc = sort.charAt(1) == 'a';
+      $btnSort = $('#btnSort'+field);
+      var t = $btnSort.text().split(" ");
+      var tipo_ord = t[0];
+      var arrow_n = ((asc) ? '\u2191' : '\u2193');
+      $btnSort.text(tipo_ord + ' ' + arrow_n);
+      $btnSort.siblings().removeClass('btn-sel').addClass('btn-not-sel');
+      $btnSort.addClass('btn-sel').removeClass('btn-not-sel');
+      $grid.isotope({sortBy: field, sortAscending: asc});
+    }
+
+    function toggleSort($target){
+      var t = $target.text().split(" ");
+      var tipo_ord = t[0];
+      var sort_t = tipo_ord.charAt(0).toLowerCase();
+      var arrow_n = "";
+      if ($target.hasClass('btn-sel')){
+        if (t[1]=='\u2191'){
+          arrow_n = '\u2193';
+        }
+        else{
+          arrow_n = '\u2191';
+        }
+      }
+      else{
+        arrow_n = t[1];
+      }
+      if (arrow_n =='\u2191'){
+        sort_o = 'a';
+      }
+      else{
+        sort_o = 'd';
+      }
+      sort = sort_t + sort_o;
+    }
+    // }}}
