a7d8477800640a20abe1515da5154860b979c619
DLongoni <dave88@focusinformatica.com>
2020-12-02 22:54:46 +0000

    file php in html

--- html/get_concerts.php
+++ html/get_concerts.php
@@ -1,14 +1,11 @@
 <?php
 require_once __DIR__ . '/../src/REP/RepEvent.php';
-$rep=new RepEvent();
-$inDt = date_create_from_format('d/m/Y',$_REQUEST['date']);
-$inDt = date_format($inDt,"j-M-Y");
-$eList=RepEvent::getByDate($inDt);
-$eArray=array();
-foreach ($eList as $e){
-  // array_push($eArray,$e->getHtml());
-  // $eArray[] = $e->getHtml();
-  echo($e->getHtml());
+$rep = new RepEvent();
+$inDt = date_create_from_format('d/m/Y', $_REQUEST['date']);
+$inDt = date_format($inDt, "j-M-Y");
+$eList = RepEvent::getByDate($inDt);
+$eArray = array();
+foreach ($eList as $e) {
+    echo($e->getHtml());
 }
-// echo json_encode($eArray);
 ?>
--- html/user_submit.php
+++ html/user_submit.php
@@ -15,62 +15,62 @@ $outcome = 0;
 // 6 -> event submitted but discarded cause of crawling problems
 $type_id = 0;
 
-if ($type=="ev"){
-  $type_id = 2;
-}
-elseif($type=="loc"){
-  $type_id = 1;
+if ($type=="ev") {
+    $type_id = 2;
+} elseif ($type=="loc") {
+    $type_id = 1;
 }
 
 try{
-  if ($type_id > 0){
+    if ($type_id > 0) {
 
-    $m = preg_match('/facebook\.[\w]{2,3}\/(events\/)?([\w\d-]+)\/?/',$link,$matches);
-    if ($m==1){
+        $m = preg_match(
+            '/facebook\.[\w]{2,3}\/(events\/)?([\w\d-]+)\/?/', 
+            $link, 
+            $matches
+        );
+        if ($m==1) {
 
-      $fb_id = $matches[2];
-      if ($type_id==1){ // Venue -> do I already have it?
-        $V = RepVenue::GetByFbLink($fb_id);
-        if ($V!=NULL){
-          $outcome=3;
-        }
-      }    
-      else if ($type_id==2){ // Event -> do I already have it?
-        $E = RepEvent::GetByFbId($fb_id);
-        if ($E!=NULL){
-          switch ($E->statusId){
-          case 1: // Loaded
-            $outcome = 2;
-            break;
-          case 2: // Approved
-            $outcome = 4;
-            break;
-          case 3: // Rejected
-            $outcome = 5;
-            break;
-          case 4: // Approved Bandless
-            $outcome = 4;
-            break;
-          }
-        }
-      }    
-      elseif (RepUserSubmissions::exists($fb_id,$type_id,2)){
-        $outcome=6; // Submitted and discarded for importing problems
-      }
-      elseif (RepUserSubmissions::exists($fb_id,$type_id)){
-        $outcome=2;
-      }    
+            $fb_id = $matches[2];
+            if ($type_id==1) { // Venue -> do I already have it?
+                $V = RepVenue::GetByFbLink($fb_id);
+                if ($V!=null) {
+                    $outcome=3;
+                }
+            } else if ($type_id==2) { // Event -> do I already have it?
+                $E = RepEvent::GetByFbId($fb_id);
+                if ($E!=null) {
+                    switch ($E->statusId){
+                    case 1: // Loaded
+                        $outcome = 2;
+                        break;
+                    case 2: // Approved
+                        $outcome = 4;
+                        break;
+                    case 3: // Rejected
+                        $outcome = 5;
+                        break;
+                    case 4: // Approved Bandless
+                        $outcome = 4;
+                        break;
+                    }
+                }
+            } elseif (RepUserSubmissions::exists($fb_id, $type_id, 2)) {
+                $outcome=6; // Submitted and discarded for importing problems
+            } elseif (RepUserSubmissions::exists($fb_id, $type_id)) {
+                $outcome=2;
+            }    
 
-      if($outcome==0){ // If i didn't find the event anywhere I add it
-        $n_id = RepUserSubmissions::add($link,$type_id);
-        if ($n_id > 0){
-          $outcome = 1;
+            if ($outcome==0) { // If i didn't find the event anywhere I add it
+                $n_id = RepUserSubmissions::add($link, $type_id);
+                if ($n_id > 0) {
+                    $outcome = 1;
+                }
+            }
         }
-      }
     }
-  }
 }
 finally{
-  echo $outcome;
+    echo $outcome;
 }
 ?>
